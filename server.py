2106be04fbe54ab2bc02073a6a1ddfe6292acfb3
Francis Tseng <f+accounts@frnsys.com>
2020-12-29 14:23:11 -0500

    separated html to gen_html()

--- server.py
+++ server.py
@@ -51,6 +51,81 @@ def gen_rss(items):
             el.text = tag
     return etree.tostring(rss)
 
+def gen_html(items):
+    # Reverse chron
+    html = ['''
+        <html>
+            <head>
+                <meta charset="utf8">
+                <style>
+                    html {
+                        overflow-x: hidden;
+                    }
+                    article {
+                        margin: 4em auto;
+                        max-width: 720px;
+                        line-height: 1.4;
+                        padding-bottom: 4em;
+                        border-bottom: 2px solid black;
+                        font-family: sans-serif;
+                    }
+                    .highlight {
+                        margin: 2em 0;
+                    }
+                    .tags {
+                        color: #888;
+                        margin-top: 1em;
+                        font-size: 0.8em;
+                    }
+                    a {
+                        color: blue;
+                    }
+                    img {
+                        max-width: 100%;
+                    }
+                </style>
+            </head>
+            <body>''']
+
+    grouped = defaultdict(list)
+    for d in items:
+        grouped[d['href']].append(d)
+
+    for href, group in sorted(grouped.items(), key=lambda g: -max([d['time'] for d in g[1]])):
+        html.append('''
+            <article>
+                <h4><a href="{href}">{title}</a></h4>'''.format(href=href, title=group[0]['title']))
+        for d in group:
+            if 'file' in d:
+                # fname = d['file']['name']
+                html.append('''
+                    <div class="highlight">
+                        <img src="{src}">
+                        <p>{text}</p>
+                        <div class="tags"><em>{tags}</em></div>
+                    </div>
+                '''.format(
+                    # src=os.path.join(args.UPLOAD_DIR, fname),
+                    src=d['file']['src'],
+                    text=d['text'],
+                    tags=', '.join(d['tags'])
+                ))
+            else:
+                html.append('''
+                    <div class="highlight">
+                        {html}
+                        <div class="tags"><em>{tags}</em></div>
+                    </div>
+                '''.format(
+                    html=d['html'],
+                    tags=', '.join(d['tags'])
+                ))
+        html.append('</article>')
+
+    html.append('</body></html>')
+    return '\n'.join(html).encode('utf8')
+
+
 class JSONRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         auth_key = self.headers.get('Authentication')
@@ -111,84 +186,8 @@ class JSONRequestHandler(BaseHTTPRequestHandler):
         else:
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
-            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
-
-            # Reverse chron
-            html = ['''
-                <html>
-                    <head>
-                        <meta charset="utf8">
-                        <style>
-                            html {
-                                overflow-x: hidden;
-                            }
-                            article {
-                                margin: 4em auto;
-                                max-width: 720px;
-                                line-height: 1.4;
-                                padding-bottom: 4em;
-                                border-bottom: 2px solid black;
-                                font-family: sans-serif;
-                            }
-                            .highlight {
-                                margin: 2em 0;
-                            }
-                            .tags {
-                                color: #888;
-                                margin-top: 1em;
-                                font-size: 0.8em;
-                            }
-                            a {
-                                color: blue;
-                            }
-                            img {
-                                max-width: 100%;
-                            }
-                        </style>
-                    </head>
-                    <body>''']
-
-            grouped = defaultdict(list)
-            for d in items:
-                grouped[d['href']].append(d)
-
-            for href, group in sorted(grouped.items(), key=lambda g: -max([d['time'] for d in g[1]])):
-                html.append('''
-                    <article>
-                        <h4><a href="{href}">{title}</a></h4>'''.format(href=href, title=group[0]['title']))
-                for d in group:
-                    if 'file' in d:
-                        # fname = d['file']['name']
-                        html.append('''
-                            <div class="highlight">
-                                <img src="{src}">
-                                <p>{text}</p>
-                                <div class="tags"><em>{tags}</em></div>
-                            </div>
-                        '''.format(
-                            # src=os.path.join(args.UPLOAD_DIR, fname),
-                            src=d['file']['src'],
-                            text=d['text'],
-                            tags=', '.join(d['tags'])
-                        ))
-                    else:
-                        html.append('''
-                            <div class="highlight">
-                                {html}
-                                <div class="tags"><em>{tags}</em></div>
-                            </div>
-                        '''.format(
-                            html=d['html'],
-                            tags=', '.join(d['tags'])
-                        ))
-                html.append('</article>')
-
-            html.append('</body></html>')
-
-            # Response
-            html = '\n'.join(html).encode('utf8')
-            self.wfile.write(html)
+            self.wfile.write(gen_html(items))
 
 
 if __name__ == '__main__':
